8dddc3c9162b73e8465def26e179abafa94153cd
judymoody59 <jmj.chai@gmail.com>
2025-03-08 16:26:16 +0900

    fix: 멤버 상세보기 스크롤 길이 조절
    - 멤버 상세보기 스크롤에서 멤버가 4명씩 보이도록 수정

--- src/app/team/section_s.js
+++ src/app/team/section_s.js
@@ -104,7 +104,7 @@ export const ToggleMemberList = styled.div.withConfig({
   border-radius: 5px;
   padding: 10px;
   width: 130px;
-  max-height: 140px; 
+  height: 215px; 
   overflow-y: auto; /* 스크롤 */
   position: absolute;
   top: 100%; 
