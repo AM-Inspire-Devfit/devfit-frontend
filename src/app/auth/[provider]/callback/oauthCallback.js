52d4f95c8b12031bea88ab7ea0d6268cab03d2ca
willjsw <sunwoo1137@gmail.com>
2025-04-13 10:57:40 +0900

    refactor: credential 설정 추가 및 요청 실패 시 홈 리디렉션

--- src/app/auth/[provider]/callback/oauthCallback.js
+++ src/app/auth/[provider]/callback/oauthCallback.js
@@ -3,11 +3,12 @@
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import axios from "axios";
+import { useAlert } from "@/context/AlertContext";
 
 const OAuthCallback = () => {
   const pathname = usePathname(); 
   const searchParams = useSearchParams(); 
-
+  const { showAlert } = useAlert();
   useEffect(() => {
     
     const provider = pathname.split("/")[2]; // ~/auth/[provider]/callback
@@ -27,13 +28,17 @@ const OAuthCallback = () => {
         const res = await axios.post("../../api/auth/social-login", {
         oauthProvider: provider.toUpperCase(),
         code: code,
-      });
+      },
+      {
+        withCredentials: true
+      })
       console.log(res.data);
       localStorage.setItem("accessToken", res.data.data.accessToken);
-      // 성공: 홈 페이지로 리다이렉트
+      //성공: 보딩 페이지로 리다이렉트
       window.location.href = "/home";
     } catch (error) {
-      console.error("토큰 요청 실패:", error.response ? error.response.data : error.message);
+      // window.location.href = "/boarding";
+      console.log("토큰 요청 실패:", error.response ? error.response.data : error.message);
     }
   };
 
