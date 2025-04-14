c790260b6bc0e5420db3fcc6a68cede5525ae435
judymoody59 <jmj.chai@gmail.com>
2025-04-14 21:19:21 +0900

    fix: 마이페이지 수정
    - [개인 페이지] 개별 회원 기여도 점수 조회 api 연결

--- src/app/api/user/userApi.js
+++ src/app/api/user/userApi.js
@@ -11,3 +11,18 @@ export const fetchUserData = async () => {
         throw new Error(message);
     }
 };
+
+
+// [개인 페이지] 개별 회원 기여도 점수 조회
+export const fetchUserContributionData = async (projectId, sprintId) => {
+    try {
+        const res = await axiosWithAuthorization.get(`/contributions/${projectId}/me`, {
+            params: { sprintId },
+        });
+        console.log("[개인 페이지] 개별 회원 기여도 점수 조회:", res.data);
+        return res.data.data;
+    } catch (error) {
+        const message = error?.response?.data?.data?.message ?? "[개인 페이지] 개별 회원 기여도 점수를 조회할 수 없습니다.";
+        throw new Error(message);
+    }
+};
